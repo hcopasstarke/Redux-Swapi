6eaa7d06a58cb04258fa2669124bc19851329ed2
Haili Copas-Starke <hcopasstarke@gmail.com>
2019-06-26 16:04:20 -0700

    finished

--- src/views/CharacterListView.js
+++ src/views/CharacterListView.js
@@ -1,35 +1,46 @@
-import React from "react";
-import { connect } from "react-redux";
+import React from "react"
+import { connect } from "react-redux"
 
-import { CharacterList } from "../components";
+import { CharacterList } from "../components"
+
 // import actions
-
+import { fetchCharacters } from "../actions/index.js"
 class CharacterListView extends React.Component {
-  constructor() {
-    super();
-  }
+	constructor() {
+		super()
+	}
 
-  componentDidMount() {
-    // call our action
-  }
+	componentDidMount() {
+		// call our action
+		this.props.fetchCharacters()
+	}
 
-  render() {
-    if (this.props.fetching) {
-      // return something here to indicate that you are fetching data
-    }
-    return (
-      <div className="CharactersList_wrapper">
-        <CharacterList characters={this.props.characters} />
-      </div>
-    );
-  }
+	render() {
+		if (this.props.fetching) {
+			// return something here to indicate that you are fetching data
+			return <p>Fetching Characters... One moment please!</p>
+		}
+		return (
+			<div className='CharactersList_wrapper'>
+				<CharacterList characters={this.props.characters} />
+			</div>
+		)
+	}
+}
+
+const mapStateToProps = state => {
+	return {
+		characters: state.charsReducer.characters,
+		loading: state.charsReducer.loading
+	}
 }
 
 // our mapStateToProps needs to have two properties inherited from state
 // the characters and the fetching boolean
 export default connect(
-  null /* mapStateToProps replaces null here */,
-  {
-    /* action creators go here */
-  }
-)(CharacterListView);
+	mapStateToProps /* mapStateToProps replaces null here */,
+	{
+		/* action creators go here */
+		fetchCharacters
+	}
+)(CharacterListView)
